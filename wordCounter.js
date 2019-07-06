33bdd97e5663feaee5145ae40937285c7a9c6c50
Saifullah <saiflll284@gmail.com>
2019-07-06 09:58:24 +0530

    word counter
    find information from a text

--- wordCounter.js
+++ wordCounter.js
@@ -1,6 +1,21 @@
 const loader = document.querySelector('.loader');
 const saif = document.querySelector('.saif');
 const container = document.querySelector('.container');
+const textarea = document.querySelector('textarea');
+const enter = document.querySelector('.enter');
+const clear = document.querySelector('.clear');
+const numOfWords = document.querySelector('.num-of-words');
+const numOfLetters = document.querySelector('.num-of-letters');
+const numOfSentences = document.querySelector('.num-of-sentences');
+const numOfSpaces = document.querySelector('.num-of-spaces');
+const number = document.querySelector('.number');
+const findNumBtn = document.querySelector('.find-num');
+const numOfXLetterWords = document.querySelector('.num-of-x-letter-words');
+const wordInput = document.querySelector('.word');
+const findWordBtn = document.querySelector('.find-word');
+const displayResult1 = document.querySelector('.display-result1');
+const displayResult2 = document.querySelector('.display-result2');
+const wordResult = document.querySelector('.word-result');
 
 
 
@@ -59,9 +74,9 @@ function doesItContain(str, word) {
     });
     
     if (num == 0) {
-        // console.log("Word not found!");
+        return "Word Not Found! :(";
     }else {
-        // console.log(`Word Found! the string contains ${num} ${word}`);
+        return `Word Found! :) <br> There are ${num} such words in the text`;
         
     }
     
@@ -75,9 +90,9 @@ function countSentences(str) {
     var num = 0;
     var text = str.trim().split(".");
     if (text[text.length-1] == "") {
-        // console.log(text.length - 1);
+        return text.length - 1;
     }else {
-        // console.log(text.length);
+        return text.length;
     }
 }    
 
@@ -96,3 +111,39 @@ setTimeout(function(){
    }, 3000);
 
 
+// when enter is clicked
+enter.addEventListener('click', displayResults);
+function displayResults() {
+    const textVal = textarea.value;
+    numOfWords.innerHTML = countWords(textVal);
+    numOfLetters.innerHTML = countLetters(textVal, "number of letters");
+    numOfSentences.innerHTML = countSentences(textVal);
+    numOfSpaces.innerHTML = countLetters(textVal, "number of spaces");
+
+}
+
+findNumBtn.addEventListener('click', () => {
+    const textVal = textarea.value;
+    const num = number.value;
+    const ans = countNumOfXLetterWords(textVal, num);
+    numOfXLetterWords.innerHTML = ans;
+})
+
+findWordBtn.addEventListener('click', () => {
+    const textVal = textarea.value;
+    const word = wordInput.value;
+    const ans = doesItContain(textVal, word); 
+    displayResult1.classList.add('invisible2');
+    displayResult2.classList.remove('invisible2');
+    wordResult.innerHTML = ans;
+    setTimeout(function(){
+        displayResult1.classList.remove('invisible2');
+        displayResult2.classList.add('invisible2');
+    },3000)
+    
+
+})
+
+clear.addEventListener('click', () => {
+    textarea.value = '';
+})
